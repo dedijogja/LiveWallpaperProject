39a7681519131726e554c170da3a56fe25e7f89a
dedijogja <dedistyawan68@yahoo.com>
2022-07-17 16:59:17 +0700

    first commit

---
+++ app/src/main/java/com/edanlab/live/PilihWallpaper.java
@@ -0,0 +1,15 @@
+package com.edanlab.live;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edanlab.live.R;
+
+public class PilihWallpaper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_wallpaper);
+    }
+}
---
+++ app/src/main/java/com/edanlab/live/PreviewWallpaper.java
@@ -0,0 +1,15 @@
+package com.edanlab.live;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edanlab.live.R;
+
+public class PreviewWallpaper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_wallpaper);
+    }
+}
